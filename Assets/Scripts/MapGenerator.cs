9075165d20874445cf6d3b1f2bc00e096c80c2be
Jan Risse <swampert_555@yahoo.com>
2017-11-03 13:59:14 -0400

    working on maps

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+    public TextAsset map;
+    public GameObject[] tiles;
+	// Use this for initialization
+	void Start () {
+        StringReader sr = new StringReader(map.text);
+        string line;
+        int x = 0, y = 0;
+        while((line = sr.ReadLine()) != null)
+        {
+            foreach(char c in line)
+            {
+                if (c == ',')
+                    continue;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
