45b41cad689824145e36966134ba280d29c0261b
Rishi Raj <rajminster@gmail.com>
2017-11-03 14:21:25 -0400

    Added basic camera controls

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraControl : MonoBehaviour {
-
+	public Camera cam;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +11,25 @@ public class CameraControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKeyUp (KeyCode.UpArrow)) {
+			cam.gameObject.transform.position += Vector3.forward;
+		}
+		else if (Input.GetKeyUp (KeyCode.DownArrow)) {
+			cam.gameObject.transform.position += Vector3.back;
+		}
+		else if (Input.GetKeyUp (KeyCode.RightArrow)) {
+			cam.gameObject.transform.position += Vector3.right;
+		}
+		else if (Input.GetKeyUp (KeyCode.LeftArrow)) {
+			cam.gameObject.transform.position += Vector3.left;
+		}
+		else if (Input.GetAxis("Mouse ScrollWheel") > 0 && cam.orthographicSize < 13) {
+			cam.orthographicSize+=0.3f;
+		}
+		else if (Input.GetAxis("Mouse ScrollWheel") < 0 && cam.orthographicSize > 2) {
+			cam.orthographicSize-=0.3f;
+		}
+
 		
 	}
 }
