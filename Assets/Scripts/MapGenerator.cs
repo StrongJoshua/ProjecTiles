ae24a7dd7995e861ccee8a8b481fa962642c14b3
Jan Risse <swampert_555@yahoo.com>
2017-11-03 14:43:47 -0400

    Added dictionary to access tile at a given position

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,11 +1,15 @@
 ﻿using System.IO;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
     public TextAsset map;
-    public GameObject[] tiles;
+    public GameObject[] tilePrefabs;
+    private Dictionary<Point, int> tiles;
 	// Use this for initialization
 	void Start () {
+        tiles = new Dictionary<Point, int>();
         StringReader sr = new StringReader(map.text);
         string line;
         int x = 0, z = 0;
@@ -15,15 +19,19 @@ public class MapGenerator : MonoBehaviour {
             {
                 if (c == ',')
                     continue;
-                Instantiate(tiles[(int)char.GetNumericValue(c)], new Vector3(x++, 0, z), Quaternion.identity);
+                int tileInt = (int)char.GetNumericValue(c);
+                Instantiate(tilePrefabs[tileInt], new Vector3(x, 0, z), Quaternion.identity);
+                tiles.Add(new Point(x, z), tileInt);
             }
             x = 0;
             z++;
         }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public int getTile(Point p)
+    {
+        int val;
+        if (tiles.TryGetValue(p, out val)) return val;
+        throw new Exception("Tile " + p + " does not exist.");
+    }
 }
---
+++ Assets/Scripts/Point.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point {
+    private int x;
+    public int X
+    {
+        get
+        {
+            return this.x;
+        }
+    }
+    private int y;
+    public int Y
+    {
+        get
+        {
+            return this.y;
+        }
+    }
+
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public override string ToString()
+    {
+        return "(" + x + ", " + y + ")";
+    }
+}
