97333b19219ff80f34bf8a3a581456901cd9d4d6
Rishi Raj <rajminster@gmail.com>
2017-11-05 01:28:16 -0400

    Interpolated camera movement, highlight movement

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -7,26 +7,39 @@ public class CameraControl : MonoBehaviour {
 	public int maxZoom;
 	public int minZoom;
 	public GameObject currentTile;
+	public GameObject highlight;
+	bool moving;
 	// Use this for initialization
 	void Start () {
-		
+		moving = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.UpArrow)) {
-			iTween.MoveTo (cam.gameObject, cam.gameObject.transform.position + Vector3.back * 3, 1f);
+		print (moving);
+		if (Input.GetKeyDown (KeyCode.UpArrow) && !moving) {
+			//moving = true;
+			highlight.transform.position += Vector3.forward * 3;
+//			iTween.MoveTo (cam.gameObject, iTween.Hash("position", cam.gameObject.transform.position + Vector3.forward * 3,"time", 0.2f, "oncomplete","unlock", 
+//				"oncompletetarget", this.gameObject, "oncompleteparams", new Hashtable()));
 		}
-		else if (Input.GetKeyDown (KeyCode.DownArrow)) {
-			iTween.MoveTo (cam.gameObject, cam.gameObject.transform.position + Vector3.forward * 3, 1f);
+		else if (Input.GetKeyDown (KeyCode.DownArrow) && !moving) {
+			//moving = true;
+			highlight.transform.position += Vector3.back * 3;
+//			iTween.MoveTo (cam.gameObject, iTween.Hash("position", cam.gameObject.transform.position + Vector3.back * 3,"time", 0.2f, "oncomplete","unlock", 
+//				"oncompletetarget", this.gameObject, "oncompleteparams", new Hashtable()));
 		}
-		else if (Input.GetKeyDown (KeyCode.RightArrow)) {
-			//cam.gameObject.transform.position += transform.TransformDirection( Vector3.right ); 
-			iTween.MoveTo (cam.gameObject, cam.gameObject.transform.position + transform.TransformDirection( Vector3.right ) * 3, 1f);
+		else if (Input.GetKeyDown (KeyCode.RightArrow) && !moving) {
+			//moving = true;
+			highlight.transform.position += Vector3.right * 3;
+//			iTween.MoveTo (cam.gameObject, iTween.Hash("position", cam.gameObject.transform.position + Vector3.right * 3,"time", 0.2f, "oncomplete","unlock", 
+//				"oncompletetarget", this.gameObject, "oncompleteparams", new Hashtable()));
 		}
-		else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-			//cam.gameObject.transform.position += transform.TransformDirection( Vector3.left );
-			iTween.MoveTo (cam.gameObject, cam.gameObject.transform.position + transform.TransformDirection( Vector3.left ) * 3, 1f);
+		else if (Input.GetKeyDown (KeyCode.LeftArrow) && !moving) {
+			//moving = true;
+			highlight.transform.position += Vector3.left * 3;
+//			iTween.MoveTo (cam.gameObject, iTween.Hash("position", cam.gameObject.transform.position + Vector3.left * 3,"time", 0.2f, "oncomplete","unlock", 
+//				"oncompletetarget", this.gameObject, "oncompleteparams", new Hashtable()));
 		}
 		else if (Input.GetAxis("Mouse ScrollWheel") > 0) {
 			iTween.MoveTo (cam.gameObject, cam.gameObject.transform.position + transform.TransformDirection( Vector3.forward ) * 6, 0.3f);
@@ -36,6 +49,7 @@ public class CameraControl : MonoBehaviour {
 			
 		}
 
+		iTween.MoveUpdate (this.gameObject, highlight.transform.position + new Vector3(0,15f,-45f), 1f);
 		
 	}
 
@@ -43,5 +57,10 @@ public class CameraControl : MonoBehaviour {
 	{
 		currentTile = tile;
 		iTween.MoveTo (cam.gameObject, tile.transform.position, 0.5f);
+	}
+	public void unlock()
+	{
+		print ("called");
+		moving = false;
 	}
 }
