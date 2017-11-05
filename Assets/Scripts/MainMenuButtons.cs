41503ba9f7e5897f04a27ba000e94d65f7011645
Jan Risse <swampert_555@yahoo.com>
2017-11-04 21:26:03 -0400

    Finished main menu

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour {
+    public GameObject mainMenuGroup, creditsGroup;
+    public AudioClip click;
+
+    public void showCredits ()
+    {
+        mainMenuGroup.SetActive(false);
+        creditsGroup.SetActive(true);
+    }
+
+    public void showMainMenu ()
+    {
+        creditsGroup.SetActive(false);
+        mainMenuGroup.SetActive(true);
+    }
+
+    public void playClick()
+    {
+        GetComponent<AudioSource>().PlayOneShot(click);
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("MapGenTest");
+    }
+}
