f4246c296f1f82f734065774ee01cfeef9880cbb
Rishi Raj <rajminster@gmail.com>
2017-11-05 17:45:01 -0500

    Credits screen should go away at the right moment now

--- Assets/Scripts/MainMenuButtons.cs
+++ Assets/Scripts/MainMenuButtons.cs
@@ -9,13 +9,14 @@ public class MainMenuButtons : MonoBehaviour {
     public GameObject mainMenuGroup, creditsGroup, backButton;
     public AudioClip click;
     public EventSystem eventSystem;
+	Rect ScreenRect = new Rect(0,0,Screen.width,Screen.height);
 
     public void showCredits ()
     {
         mainMenuGroup.SetActive(false);
         creditsGroup.SetActive(true);
 		backButton.SetActive (true);
-		creditsGroup.GetComponent<RectTransform> ().position = new Vector3 (Screen.width/2f, -Screen.height/3f, 0);
+		creditsGroup.GetComponent<RectTransform> ().position = new Vector3 (Screen.width/2f, -Screen.height/2f, 0);
 		eventSystem.SetSelectedGameObject(backButton);
     }
 
@@ -24,7 +25,7 @@ public class MainMenuButtons : MonoBehaviour {
         creditsGroup.SetActive(false);
 		backButton.SetActive (false);
         mainMenuGroup.SetActive(true);
-		creditsGroup.GetComponent<RectTransform> ().position = new Vector3 (Screen.width/2f, -Screen.height/3f, 0);
+		creditsGroup.GetComponent<RectTransform> ().position = new Vector3 (Screen.width/2f, -Screen.height/2f, 0);
         eventSystem.SetSelectedGameObject(mainMenuGroup.GetComponentInChildren<Button>().gameObject);
     }
 
@@ -43,7 +44,9 @@ public class MainMenuButtons : MonoBehaviour {
 		if (creditsGroup.activeSelf) {
 			creditsGroup.GetComponent<RectTransform>().position += new Vector3 (0,Screen.height/10f*Time.deltaTime,0);
 		}
-		if (creditsGroup.GetComponent<RectTransform> ().position.y > Screen.height * 1.55f) {
+		Vector3[] worldCorners = new Vector3[4];
+		creditsGroup.GetComponent<RectTransform> ().GetWorldCorners (worldCorners);
+		if (!ScreenRect.Contains(worldCorners[0]) && worldCorners[0].y > 0 && creditsGroup.GetComponent<RectTransform> ().position.y > 0) {
 			showMainMenu ();
 		}
 	}
