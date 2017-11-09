d4852439744e3baa0e395b7b757cfc6a6c543e88
Rishi Raj <rajminster@gmail.com>
2017-11-09 18:58:31 -0500

    Projectile Firing Tests

---
+++ Assets/Scripts/FireProjectile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireProjectile : MonoBehaviour {
+	public GameObject projectile;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.F)) {
+			GameObject temp = Instantiate (projectile);
+			temp.transform.position = transform.position + new Vector3 (0,1,0);
+			temp.GetComponent<Rigidbody> ().AddForce (new Vector3(0,0,100));
+		}
+			
+	}
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+	public float lifetime;
+	float startTime;
+	// Use this for initialization
+	void Start () {
+		startTime = Time.timeSinceLevelLoad;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.timeSinceLevelLoad - startTime > lifetime) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnTriggerEnter()
+	{
+	}
+}
