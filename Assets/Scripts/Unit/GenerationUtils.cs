379ef083af1c05708ec4a12ea0334e76c307704d
Jan Risse <swampert_555@yahoo.com>
2017-11-16 15:47:13 -0500

    Color lerping

--- Assets/Scripts/Unit/GenerationUtils.cs
+++ Assets/Scripts/Unit/GenerationUtils.cs
@@ -7,11 +7,12 @@ public class GenerationUtils : MonoBehaviour {
     public static void setColor(GameObject obj, Color col)
     {
         SkinnedMeshRenderer[] renderers = obj.GetComponentsInChildren<SkinnedMeshRenderer>();
+        MeshRenderer[] meshes = obj.GetComponentsInChildren<MeshRenderer>();
 
-        if (renderers.Length > 0)
-        {
-            foreach(SkinnedMeshRenderer smr in renderers)
-                smr.material.color = col;
-        }
+        foreach (SkinnedMeshRenderer smr in renderers)
+            smr.material.color = Color.Lerp(smr.material.color, col, .5f);
+        foreach (MeshRenderer mr in meshes)
+            foreach (Material m in mr.materials)
+                m.color = Color.Lerp(m.color, col, .5f);
     }
 }
