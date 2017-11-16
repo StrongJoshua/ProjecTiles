4eb6403cd18a3c506a0f0f4e72e76914e4479254
Jan Risse <swampert_555@yahoo.com>
2017-11-16 16:48:44 -0500

    Shoot is properly disabled

---
+++ Assets/Scripts/InteractableButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InteractableButton : MonoBehaviour {
+    public EventSystem eventSystem;
+    Button button;
+
+	// Use this for initialization
+	void Start () {
+        button = this.GetComponentInChildren<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (eventSystem.currentSelectedGameObject == button.gameObject && !button.IsInteractable())
+            eventSystem.SetSelectedGameObject(button.FindSelectableOnUp().gameObject);
+	}
+}
