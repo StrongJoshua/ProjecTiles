0d39bb390dda965415c13448645dece5b8cb658c
Aaron Andrews <superryuk@gmail.com>
2017-11-17 04:51:26 -0500

    Health and AP bars always look at camera and overlay everything now

--- Assets/Scripts/Unit/APBar.cs
+++ Assets/Scripts/Unit/APBar.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class APBar : MonoBehaviour {
 
     private int offset = 3;
     public float yOffset = 0;
     Vector3 yFreeze;
+	public Material mat;
 	// Use this for initialization
 	void Start () {
         yFreeze = new Vector3(1f, 0f, 1f);
+		Image barHolder = transform.GetChild (0).gameObject.GetComponent<Image> ();
+		Image barAmount = transform.GetChild (1).gameObject.GetComponent<Image> ();
+
+		if (mat != null) {
+			barHolder.material = mat;
+			barAmount.material = mat;
+		}
 	}
 	
 	// Update is called once per frame
@@ -22,6 +31,7 @@ public class APBar : MonoBehaviour {
         newPos.z = transform.parent.position.z + Mathf.Sin(Mathf.Atan2(vec.z, vec.x)) * 2;
         transform.position = newPos;
 
-        transform.rotation = Quaternion.LookRotation(Camera.main.transform.position) * Quaternion.Euler(0, 0, 0);
+        //transform.rotation = Quaternion.LookRotation(Camera.main.transform.position) * Quaternion.Euler(0, 0, 0);
+		transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
 	}
 }
