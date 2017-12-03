20010eb0be8370dd70e68723a44d52a04606332c
David <riazati.david@gmail.com>
2017-12-03 02:01:09 -0500

    rock healthbar

--- Assets/Scripts/RockManager.cs
+++ Assets/Scripts/RockManager.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RockManager : MonoBehaviour {
 
     public GameObject[] smallRocks, medRocks, bigRocks;
 
     enum RockType { BIG, MED, SMALL }
+
+    public Image healthBar;
+    public GameObject healthBarContainer;
     public GameObject currRock;
     RockType currType;
-    public int currHealth;
+    public int currHealth, maxHealth;
 
     public int bigRockHealth;
     public int medRockHealth;
@@ -31,6 +35,7 @@ public class RockManager : MonoBehaviour {
         currRock.transform.localScale = new Vector3(0.5f, 1.575f, 0.499f);
         currType = RockType.BIG;
         currHealth = bigRockHealth;
+        maxHealth = bigRockHealth;
         lastHitBy = null;
         //currRock.transform.position = new Vector3(0.002f, 0.75f, -0.002f);
 
@@ -51,12 +56,16 @@ public class RockManager : MonoBehaviour {
         {
             nextRock = choose(medRocks);
             currHealth = medRockHealth;
+            maxHealth = medRockHealth;
             currType = RockType.MED;
+            healthBar.rectTransform.localScale = new Vector3(1f, 1f, 1f);
         }
         else if (currType == RockType.MED)
         {
             nextRock = choose(smallRocks);
             currHealth = smallRockHealth;
+            healthBar.rectTransform.localScale = new Vector3(1f, 1f, 1f);
+            maxHealth = smallRockHealth;
             currType = RockType.SMALL;
         }
         else if (currType == RockType.SMALL)
@@ -64,6 +73,7 @@ public class RockManager : MonoBehaviour {
             // reset movement cost
             Tile tile = GameObject.FindGameObjectWithTag("MapGenerator").GetComponent<MapGenerator>().GetTile(tileX, tileY);
             tile.MovementCost = Tile.Plain.MovementCost;
+            healthBarContainer.SetActive(false);
             return;
         }
 
@@ -77,6 +87,9 @@ public class RockManager : MonoBehaviour {
         lastHitBy = projectile;
 
         currHealth -= damage;
+        float scale = (float)currHealth / maxHealth;
+        if (scale <= 0) scale = 0;
+        healthBar.rectTransform.localScale = new Vector3(scale, 1f, 1f);
         if (currHealth <= 0)
         {
             remove();
