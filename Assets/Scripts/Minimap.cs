626b4a557fdf07a2c9584ccf69c7ab4032b85ecd
Rajminster <rajminster@gmail.com>
2017-12-04 19:56:19 -0500

    Started work on minimap

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour {
+	public Camera cam;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//cam.rect = new Rect(.25f, 0.0f, 0f, .25f);
+	}
+}
