3ff7303b9c0d692825a29d7d6fad4ca1d5677175
Rishi Raj <rajminster@gmail.com>
2017-12-05 18:26:29 -0500

    Few changes to minimap

--- Assets/Scripts/Minimap.cs
+++ Assets/Scripts/Minimap.cs
@@ -23,14 +23,21 @@ public class Minimap : MonoBehaviour {
 			foreach (ArrayList arr in tiles) {
 				foreach (int k in arr) {
 					minimap.SetPixel (x, y, tileset.GetPixel(k,1));
-					print (minimap.GetPixel (x,y).ToString() + "");
+					//print (minimap.GetPixel (x,y).ToString() + "");
 					x++;
 				}
 				y++;
+				x = 0;
 			}
-			created = true;
-			gameObject.AddComponent<RawImage> ();
-			GetComponent<RawImage> ().texture = minimap;
+			Color[] colors  = new Color[minimap.height*minimap.width];
+			for (int i = 0; i < colors.Length; i++) {
+				colors [i] = new Color (0.5f, 0.5f, 0.5f,1f);
+			}
+			minimap.SetPixels (colors);
+			print(x + " " + y + " " + minimap.height);
+			//created = true;
+			//gameObject.AddComponent<RawImage> ();
+			GetComponent<RawImage> ().texture = minimap as Texture;
 
 		}
 	}
