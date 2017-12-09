d7271e609c081798c955a4f1aecabcab6afb04e7
David <david.riazati@gatech.edu>
2017-12-09 13:14:05 -0500

    added ybot animations

---
+++ Assets/Models/ModelsAndAnimations/YBot with Animations/Y_Bot/FootStep.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootStep : MonoBehaviour {
+
+    public AudioSource leftSound, rightSound;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FootstepEvent()
+    {
+        
+        leftSound.Play();
+
+      //  Debug.Log("HEY WHAT UP");
+    }
+
+    void FootstepLeftEvent()
+    {
+        if (!leftSound.isPlaying && !rightSound.isPlaying)
+            leftSound.Play();
+    }
+
+    void FootstepRightEvent()
+    {
+        if (!rightSound.isPlaying && !leftSound.isPlaying)
+            rightSound.Play();
+    }
+}
