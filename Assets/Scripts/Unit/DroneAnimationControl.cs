6bb6c2b3ab4e060866926980fbf64332a1cc3afe
David <david.riazati@gatech.edu>
2017-12-09 13:22:04 -0500

    added control script

---
+++ Assets/Scripts/Unit/DroneAnimationControl.cs
@@ -0,0 +1,235 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+//require some things the bot control needs
+[RequireComponent(typeof(Animator), typeof(Rigidbody), typeof(CapsuleCollider))]
+public class DroneAnimationControl : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody rbody;
+
+    private Transform leftFoot;
+    private Transform rightFoot;
+
+    public int groundContacts = 0;
+
+    private float filteredForwardInput = 0f;
+    private float filteredTurnInput = 0f;
+
+    public float forwardInputFilter = 5f;
+    public float turnInputFilter = 5f;
+
+    private float forwardSpeedLimit = 1f;
+
+    public bool IsGrounded
+    {
+        get { return groundContacts > 0; }
+    }
+
+
+    void Awake()
+    {
+
+        anim = GetComponent<Animator>();
+
+        if (anim == null)
+            Debug.Log("Animator could not be found");
+
+        rbody = GetComponent<Rigidbody>();
+
+        if (rbody == null)
+            Debug.Log("Rigid body could not be found");
+
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //example of how to get access to certain limbs
+        leftFoot = this.transform.Find("mixamorig:Hips/mixamorig:LeftUpLeg/mixamorig:LeftLeg/mixamorig:LeftFoot");
+        rightFoot = this.transform.Find("mixamorig:Hips/mixamorig:RightUpLeg/mixamorig:RightLeg/mixamorig:RightFoot");
+
+        if (leftFoot == null || rightFoot == null)
+            Debug.Log("One of the feet could not be found");
+
+    }
+
+
+
+
+
+    //Update whenever physics updates with FixedUpdate()
+    //Updating the animator here should coincide with "Animate Physics"
+    //setting in Animator component under the Inspector
+    void FixedUpdate()
+    {
+
+        //GetAxisRaw() so we can do filtering here instead of the InputManager
+        float h = Input.GetAxisRaw("Horizontal");// setup h variable as our horizontal input axis
+        float v = Input.GetAxisRaw("Vertical");	// setup v variables as our vertical input axis
+
+
+        //enforce circular joystick mapping which should coincide with circular blendtree positions
+        Vector2 vec = Vector2.ClampMagnitude(new Vector2(h, v), 1.0f);
+
+        h = vec.x;
+        v = vec.y;
+
+
+        //BEGIN ANALOG ON KEYBOARD DEMO CODE
+        if (Input.GetKey(KeyCode.Q))
+            h = -0.5f;
+        else if (Input.GetKey(KeyCode.E))
+            h = 0.5f;
+
+        if (Input.GetKeyUp(KeyCode.Alpha1))
+            forwardSpeedLimit = 0.1f;
+        else if (Input.GetKeyUp(KeyCode.Alpha2))
+            forwardSpeedLimit = 0.2f;
+        else if (Input.GetKeyUp(KeyCode.Alpha3))
+            forwardSpeedLimit = 0.3f;
+        else if (Input.GetKeyUp(KeyCode.Alpha4))
+            forwardSpeedLimit = 0.4f;
+        else if (Input.GetKeyUp(KeyCode.Alpha5))
+            forwardSpeedLimit = 0.5f;
+        else if (Input.GetKeyUp(KeyCode.Alpha6))
+            forwardSpeedLimit = 0.6f;
+        else if (Input.GetKeyUp(KeyCode.Alpha7))
+            forwardSpeedLimit = 0.7f;
+        else if (Input.GetKeyUp(KeyCode.Alpha8))
+            forwardSpeedLimit = 0.8f;
+        else if (Input.GetKeyUp(KeyCode.Alpha9))
+            forwardSpeedLimit = 0.9f;
+        else if (Input.GetKeyUp(KeyCode.Alpha0))
+            forwardSpeedLimit = 1.0f;
+        //END ANALOG ON KEYBOARD DEMO CODE  
+
+
+        //do some filtering of our input as well as clamp to a speed limit
+        filteredForwardInput = Mathf.Clamp(Mathf.Lerp(filteredForwardInput, v,
+                Time.deltaTime * forwardInputFilter), -forwardSpeedLimit, forwardSpeedLimit);
+
+        filteredTurnInput = Mathf.Lerp(filteredTurnInput, h,
+            Time.deltaTime * turnInputFilter);
+
+        //finally pass the processed input values to the animator
+        anim.SetFloat("velx", filteredTurnInput);	// set our animator's float parameter 'Speed' equal to the vertical input axis				
+        anim.SetFloat("vely", filteredForwardInput); // set our animator's float parameter 'Direction' equal to the horizontal input axis		
+
+        updateFall();
+
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("Idle Turn"))
+            {
+                anim.SetTrigger("ThrowNoMask");
+            }
+            else
+            {
+                anim.SetTrigger("ThrowThing");
+            }
+        }    //normally left-ctrl on keyboard
+
+    }
+
+    void updateFall()
+    {
+        bool isFalling = !IsGrounded;
+
+        if (isFalling)
+        {
+
+            const float rayOriginOffset = 1f; //origin near bottom of collider, so need a fudge factor up away from there
+            const float rayDepth = 1f; //how far down will we look for ground?
+            const float totalRayLen = rayOriginOffset + rayDepth;
+
+            Ray ray = new Ray(this.transform.position + Vector3.up * rayOriginOffset, Vector3.down);
+
+            //visualize ray in the editor
+            //I'm using DrawLine because Debug.DrawRay() doesn't allow setting ray length past a certain size
+            Debug.DrawLine(ray.origin, ray.origin + ray.direction * totalRayLen, Color.green);
+
+            RaycastHit hit;
+
+
+            //Cast ray and look for ground. If ground is close, then transition out of falling animation
+            if (Physics.Raycast(ray, out hit, totalRayLen))
+            {
+                if (hit.collider.gameObject.CompareTag("ground"))
+                {
+                    isFalling = false; //turning falling back off because we are close to the ground
+
+                    //draw an X that denotes where ray hit
+                    const float ZBufFix = 0.01f;
+                    const float edgeSize = 0.2f;
+                    Color col = Color.red;
+
+                    Debug.DrawRay(hit.point + Vector3.up * ZBufFix, Vector3.forward * edgeSize, col);
+                    Debug.DrawRay(hit.point + Vector3.up * ZBufFix, Vector3.left * edgeSize, col);
+                    Debug.DrawRay(hit.point + Vector3.up * ZBufFix, Vector3.right * edgeSize, col);
+                    Debug.DrawRay(hit.point + Vector3.up * ZBufFix, Vector3.back * edgeSize, col);
+                }
+            }
+        }
+
+
+        anim.SetBool("IsFalling", isFalling);
+    }
+
+
+    //This is a physics callback
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.transform.gameObject.tag == "ground")
+        {
+            ++groundContacts;
+
+            //Debug.Log("Player hit the ground at: " + collision.impulse.magnitude);
+
+            if (collision.impulse.magnitude > 100f)
+            {
+                // landing
+                //EventManager.TriggerEvent<PlayerLandsEvent, Vector3>(collision.contacts[0].point);
+            }
+        }
+
+    }
+
+    //This is a physics callback
+    void OnCollisionExit(Collision collision)
+    {
+
+        if (collision.transform.gameObject.tag == "ground")
+            --groundContacts;
+    }
+
+
+
+    void OnAnimatorMove()
+    {
+        if (IsGrounded)
+        {
+            //use root motion as is if on the ground		
+            this.transform.position = anim.rootPosition;
+
+        }
+        else
+        {
+            //Simple trick to keep model from climbing other rigidbodies that aren't the ground
+            this.transform.position = new Vector3(anim.rootPosition.x, this.transform.position.y, anim.rootPosition.z);
+        }
+
+        //use rotational root motion as is
+        this.transform.rotation = anim.rootRotation;
+
+    }
+
+}
