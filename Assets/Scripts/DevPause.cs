7bf302b66ea402dfaec87dce5451948d85da85cd
Rishi Raj <rajminster@gmail.com>
2017-12-09 17:28:13 -0500

    Added Dev Pause with 0 and 1 to unpause

---
+++ Assets/Scripts/DevPause.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevPause : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Alpha0))
+			Time.timeScale = 0;
+		if (Input.GetKeyDown (KeyCode.Alpha1))
+			Time.timeScale = 1;
+		
+	}
+}
