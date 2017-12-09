d579ee9eea9f2576c7f29ac189e0ac4260ef5176
Rishi Raj <rajminster@gmail.com>
2017-12-09 17:12:21 -0500

    Added Studio Scene

---
+++ Assets/Scripts/StudioCameraRotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StudioCameraRotate : MonoBehaviour {
+	public Transform point;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround (point.position, Vector3.up, Time.deltaTime * speed);
+		
+	}
+}
