a5b66b22977a8b4ad26d684b6b94a428fb5a437c
David <david.riazati@gatech.edu>
2017-12-09 21:57:12 -0500

    fixed health/ap bar rotating

--- Assets/Scripts/Unit/APBar.cs
+++ Assets/Scripts/Unit/APBar.cs
@@ -9,8 +9,9 @@ public class APBar : MonoBehaviour {
     public float xOffset = 0;
     Vector3 yFreeze;
 	public Material mat;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    private Quaternion rot;
+    void Start () {
         yFreeze = new Vector3(1f, 0f, 1f);
 		Image barHolder = transform.GetChild (0).gameObject.GetComponent<Image> ();
 		Image barAmount = transform.GetChild (1).gameObject.GetComponent<Image> ();
@@ -19,19 +20,22 @@ public class APBar : MonoBehaviour {
 			barHolder.material = mat;
 			barAmount.material = mat;
 		}
+        rot = transform.rotation;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        return;
         float oldY = transform.position.y;
-        Vector3 vec = (Camera.main.transform.position - transform.parent.position);
+        Vector3 vec = (Camera.main.transform.position - transform.parent.parent.position);
         Vector3 newPos = new Vector3(0f, 0f, 0f);
         newPos.y = oldY;
-        newPos.x = transform.parent.position.x + Mathf.Cos(Mathf.Atan2(vec.z, vec.x)) * 2 + xOffset;
-        newPos.z = transform.parent.position.z + Mathf.Sin(Mathf.Atan2(vec.z, vec.x)) * 2;
+        newPos.x = transform.parent.parent.position.x + Mathf.Cos(Mathf.Atan2(vec.z, vec.x)) * 2 + 0f;
+        newPos.z = transform.parent.parent.position.z + Mathf.Sin(Mathf.Atan2(vec.z, vec.x)) * 2;
         transform.position = newPos;
-
+        //transform.rotation = rot;
         //transform.rotation = Quaternion.LookRotation(Camera.main.transform.position) * Quaternion.Euler(0, 0, 0);
-		transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
-	}
+        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
+    }
 }
---
+++ Assets/Scripts/Unit/UnitUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitUI : MonoBehaviour {
+
+    Transform unitTransform;
+    Vector3 diff;
+    public float offset;
+    public float yOffset;
+
+	// Use this for initialization
+	void Start () {
+        unitTransform = transform.parent;
+        diff = transform.parent.position - transform.position;
+        transform.parent = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = new Vector3(unitTransform.position.x - offset, unitTransform.position.y - yOffset, unitTransform.position.z);
+        transform.position = pos + diff;
+        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
+    }
+}
