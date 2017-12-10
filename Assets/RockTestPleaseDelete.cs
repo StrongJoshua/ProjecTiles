728c86a5c453462e01738a50e6ad911dc7744cad
David <david.riazati@gatech.edu>
2017-12-09 21:04:11 -0500

    small rocks blow up from boulder

---
+++ Assets/RockTestPleaseDelete.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockTestPleaseDelete : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().AddForce(new Vector3(0f, 600f, 0f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
